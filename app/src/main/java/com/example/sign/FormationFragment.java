7e37b0177c1c49950a1b743c98e500b4a8b53336
HyrvinCoulon <bangolamoukogho@gmail.com>
2021-02-24 15:30:39 +0100

    fragment actions 01

---
+++ app/src/main/java/com/example/sign/FormationFragment.java
@@ -0,0 +1,25 @@
+package com.example.sign;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FormationFragment extends Fragment {
+
+
+
+    public FormationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_formation, container, false);
+    }
+}
