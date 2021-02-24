1c4a95dc1a5d1f80c2895f0ccf1118ada152469b
HyrvinCoulon <bangolamoukogho@gmail.com>
2021-02-24 15:30:18 +0100

    bottomnavigation features

--- app/src/main/java/com/example/sign/HomeActivity.java
+++ app/src/main/java/com/example/sign/HomeActivity.java
@@ -1,31 +1,77 @@
 package com.example.sign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottom = findViewById(R.id.bottom);
+        bottom.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.layout_frame
+                        ,new HomeFragment()).commit();
+
     }
 
-    public void logout(View view) {
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
 
-        SessionManagement sessionManagement = new SessionManagement(HomeActivity.this);
-        sessionManagement.removeSession();
+            switch (item.getItemId()){
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.formation:
+                    selectedFragment = new FormationFragment();
+                    break;
+                case R.id.presentation:
+                    selectedFragment = new AboutFragment();
+            }
 
-        movetoMain();
-    }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.layout_frame
+                            ,selectedFragment).commit();
 
-    private void movetoMain() {
-        Intent i = new Intent(HomeActivity.this, MainActivity.class );
-        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(i);
+            return true;
+        }
+    };
+
+    private void Login() {// méthode d'enregistrement
+
+        //StringRequest request = new ;
+
     }
 
 }
