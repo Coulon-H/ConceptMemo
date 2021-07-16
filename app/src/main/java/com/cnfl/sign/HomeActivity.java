73f6e66396acff3ba01f5c67075bcfd08b8d8074
HyrvinCoulon <bangolamoukogho@gmail.com>
2021-07-16 18:15:00 +0100

    Usable App

--- app/src/main/java/com/cnfl/sign/HomeActivity.java
+++ app/src/main/java/com/cnfl/sign/HomeActivity.java
@@ -7,17 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     BottomNavigationView bottom;
     ArrayList<Formation> list;
     ProgressDialog progressDialog;
+    static int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +37,18 @@ public class HomeActivity extends AppCompatActivity {
                 .replace(R.id.layout_frame
                         ,new HomeFragment(
                                 v -> {
+                                    i=1;
                                     getSupportFragmentManager().beginTransaction()
                                             .replace(R.id.layout_frame
                                                     ,new FormationFragment(progressDialog, "CNFL")).commit();
                                 },
                                 v -> {
+                                    i=1;
                                     getSupportFragmentManager().beginTransaction()
                                             .replace(R.id.layout_frame
                                                     ,new FormationFragment(progressDialog, "INCUBATEUR")).commit();
                                 },
-                                v -> {
+                                v -> {i=1;
                                     getSupportFragmentManager().beginTransaction()
                                             .replace(R.id.layout_frame
                                                     ,new FormationFragment(progressDialog, "CEF")).commit();
@@ -54,21 +60,22 @@ public class HomeActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
         Fragment selectedFragment = null;
-
-
         switch (item.getItemId()){
             case R.id.home:
                 selectedFragment = new HomeFragment(v -> {
+                    i=1;
                     getSupportFragmentManager().beginTransaction()
                             .replace(R.id.layout_frame
                                     ,new FormationFragment(progressDialog, "CNFL")).commit();
                 },
                         v -> {
+                            i=1;
                             getSupportFragmentManager().beginTransaction()
                                     .replace(R.id.layout_frame
                                             ,new FormationFragment(progressDialog, "INCUBATEUR")).commit();
                         },
                         v -> {
+                            i=1;
                             getSupportFragmentManager().beginTransaction()
                                     .replace(R.id.layout_frame
                                             ,new FormationFragment(progressDialog, "CEF")).commit();
@@ -84,5 +91,64 @@ public class HomeActivity extends AppCompatActivity {
 
         return true;
     };
+
+    @Override
+    public void onClick(View v) {
+        Fragment selectedFragment = null;
+        i = 1;
+        selectedFragment = new HomeFragment(v2 -> {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.layout_frame
+                                    ,new FormationFragment(progressDialog, "CNFL")).commit();
+                },
+                        v2 -> {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.layout_frame
+                                            ,new FormationFragment(progressDialog, "INCUBATEUR")).commit();
+                        },
+                        v2 -> {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.layout_frame
+                                            ,new FormationFragment(progressDialog, "CEF")).commit();
+                        }
+                );
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.layout_frame
+                        ,selectedFragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(i == 1){
+            Fragment selectedFragment = null;
+            i = 0;
+            selectedFragment = new HomeFragment(v2 -> {
+                i=1;
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.layout_frame
+                                ,new FormationFragment(progressDialog, "CNFL")).commit();
+            },
+                    v2 -> {i=1;
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.layout_frame
+                                        ,new FormationFragment(progressDialog, "INCUBATEUR")).commit();
+                    },
+                    v2 -> {i=1;
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.layout_frame
+                                        ,new FormationFragment(progressDialog, "CEF")).commit();
+                    }
+            );
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.layout_frame
+                            ,selectedFragment).commit();
+
+        }else{
+            super.onBackPressed();
+        }
+    }
+
 
 }
